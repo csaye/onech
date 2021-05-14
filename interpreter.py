a7ddeec8cedaeaf846307f9c09138e1449ffe64e
Cooper Saye <coopersaye@gmail.com>
2021-05-13 19:31:34 -0600

    Implement pointer and variable incrementing.

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,25 @@
 import sys, os
 
+# attributes
+vars = 256
+
+# initialize varlist and pointer
+varlist = []
+for i in range(vars): varlist.append(0)
+pointer = 0
+
+# returns whether file exists at given path
 def file_exists(path): return os.path.exists(path)
+
+# parses given character
+def parse(ch):
+    global pointer
+    if ch == '<': pointer = vars - 1 if pointer == 0 else pointer - 1
+    elif ch == '>': pointer = 0 if pointer == vars - 1 else pointer + 1
+    elif ch == '-': varlist[pointer] -= 1
+    elif ch == '+': varlist[pointer] += 1
+    elif ch == '.': print(chr(varlist[pointer]), end='')
+    elif ch == ':': print(varlist[pointer])
 
 # get program args
 args = sys.argv
